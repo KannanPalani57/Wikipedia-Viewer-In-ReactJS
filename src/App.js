e6c490cd247f76e0c4224c4ca73766c9ccf978b1
KannanPalani57 <kannanpalani455@gmail.com>
2020-09-29 22:37:35 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+ //The input field data state goes here.
+  const [searchItem, setSearchItem] = useState("");
+  
+ 
+ 
+ //make request and get data here
+  const makeRequestAndShow = e => {
+    e.preventDefault();   //preventing to the same screen
+    console.log(searchItem);
+
+
+
+    //set the field to empty after search
+    //setSearchItem("");
+  }
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "field-wrapper">
+      <div className = "field">
+         <h3>Wikipedia Viewer</h3>
+         <input type = "field" value = {searchItem} onChange = {e => setSearchItem(e.target.value)}/>
+          <button onClick = {makeRequestAndShow}>Search</button>
+      </div>
+
     </div>
   );
 }
